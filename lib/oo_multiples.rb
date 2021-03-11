46bc8a46ca3a12d797c93cc358c08f302f40b0c6
Diana Liao <dijali@gmail.com>
2021-03-11 09:39:07 -0500

    commit tree dc832aad45ec76358d4ae47216d1b5dfb7a03014

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -2,7 +2,7 @@
 
 class Multiples
 
-  attr_reader :numbers
+  attr_reader :numbers#, :limit
 
   def initialize(limit)
     number_range = (1...limit).to_a
@@ -20,5 +20,18 @@ class Multiples
   def sum_multiples
     numbers.sum
   end
+
+  # def initialize(limit)
+  #   @limit = limit
+  #   @numbers = collect_multiples
+  # end
+
+  # def collect_multiples
+  #   (1...limit).to_a.select{|num| num % 3 == 0 || num % 5 == 0}
+  # end
+
+  # def sum_multiples
+  #   numbers.sum
+  # end
 
 end
